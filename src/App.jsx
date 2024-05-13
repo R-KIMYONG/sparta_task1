1ab08b7fb386951d7c4e3493ad7745cb56d0e3be
kimyong <right4570@naver.com>
2024-05-13 20:09:51 +0900

    과제1완성

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,25 @@ function App() {
   const [users, setUsers] = useState(initialState);
 
   // TODO: 이름과 나이를 각각 상태로 정의하세요. 초기값은 빈문자열("")입니다.
-
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
   const addUser = (e) => {
     e.preventDefault();
     // TODO: 이름과 나이가 모두 입력되지 않았을 때는 alert 처리하고 함수를 종료하세요. 논리합연산자 (||) 를 이용하세요.
+    if (name === "" || age === "") {
+      alert("정보를 입력하세요");
+      return;
+    }
+    setUsers([...users, { id: Date.now(), name, age }]);
 
     // TODO: 사용자 리스트 상태를 업데이트 하세요. spread operator 를 사용하고, 추가되는 id는 현재 시간을 밀리초 단위로 반환하는 Date.now() 를 사용하세요.
   };
 
   const removeUser = (id) => {
     // TODO: filter 메소드를 사용해서 사용자 삭제 로직을 구현해 보세요.
+    let newUsers = users.filter((user) => user.id != id.target.parentNode.id);
+
+    setUsers(newUsers);
   };
 
   return (
@@ -26,16 +35,42 @@ function App() {
       <h1>사용자 리스트</h1>
       <form onSubmit={addUser}>
         {/* TODO: input 태그에 value, onChange 속성을 추가해서 이름과 나이의 상태와 상태변경 로직을 연결하세요 */}
-        <input type="text" placeholder="이름" />
-        <input type="number" placeholder="나이" />
+        <input
+          type="text"
+          placeholder="이름"
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="나이"
+          value={age}
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
         <button type="submit">사용자 추가</button>
       </form>
       <ul>
         {/* TODO: map 메소드를 이용해서 user 리스트를 렌더링하세요.  */}
-        {/* 이름: John, 나이: 20 [삭제] 버튼이 하나의 행에 나올 수 있도록 해보세요. (hint: flex) */}
+        {users.map((item) => (
+          <Lilist key={item.id} item={item} removeUser={removeUser} />
+        ))}
       </ul>
     </>
   );
 }
 
 export default App;
+
+function Lilist({ item, removeUser }) {
+  return (
+    <li id={item.id}>
+      <p>이름 : {item.name}</p>
+      <p>나이 : {item.age}</p>
+      <button onClick={removeUser}>삭제</button>
+    </li>
+  );
+}
